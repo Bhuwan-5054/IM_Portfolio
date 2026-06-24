6a7cfb4b7a4061e0fdc53b02b60953e4c00f1feb
@bhuwan5054 <119479962+Bhuwan-5054@users.noreply.github.com>
2026-06-24 17:30:00 +0530

    Update routes.ts

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,4 @@
 export const ROUTES = {
-  creator: "https://Bhuwan_kirnapure.8889607226.com",
-  cyber: "https://bhuwan5054.8889607226.com",
+  creator: "https://Bhuwan_kirnapure.8889607226.xyz",
+  cyber: "https://bhuwan5054.8889607226.xyz",
 };
